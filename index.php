bf3d15f507cd246903fddc2137fccf804aed5594
exdo <raiderex.7@gmail.com>
2013-10-18 01:01:10 +0700

    header

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php require("include/header.php");?>
+
+		<div id="carousel">
+			<div class="container">
+				<div class="row">
+					<div class="span5">
+						<img src="">
+					</div>
+					<div class="span7">
+						<div>
+							<h1>The best concrete nail</h1>
+						</div>
+						<div>
+							<p>Some detail about this topic. It should be around 3-4 lines. Some detail about this topic. It should be around 3-4 lines.</p>
+						</div>
+						<div>
+							<input type="button" value="more detail">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!--banner-->
+<?php require("include/footer.php");?>
